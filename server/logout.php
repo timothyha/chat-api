de897ac6697511bcc7743c3dfc846c8e79adb4be
Timothy Ha <timothyha@gmail.com>
2014-12-16 16:39:38 +0300

    Added logout

---
+++ server/logout.php
@@ -0,0 +1,24 @@
+<?php
+
+# Test: curl --data "session=value1" https://jesuschrist.ru/chapi/logout
+
+include "common.php";
+
+$sid = intval($_POST['session']);
+list($login, $nextlogin, $color) = check_user_session($sid);
+
+try {
+
+    $q = $db->prepare("update chatusers set conline = 0, cpresence = ?, sid = 0 where sid = ?");
+    $stamp = time();
+    $q->bindValue(1, $stamp, PDO::PARAM_INT);
+    $q->bindValue(2, $sid, PDO::PARAM_INT);
+    $q->execute();
+
+    echo json_encode(array("stamp"=>$stamp));
+
+} catch(PDOException $e) {
+
+    echo json_encode(array("err"=>"ERR_MYSQL_ERROR"));
+
+}
