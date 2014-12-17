0ed009dad7da5d7ba0a79bf1b780759974fc8dad
Grabli66 <grabli66@gmail.com>
2014-12-17 17:00:09 +0400

    smile popup

--- client/www/js/msend-panel.js
+++ client/www/js/msend-panel.js
@@ -15,7 +15,9 @@ function newSendPanel(id) {
     self.workplace.append(
             '<table cellpadding="0" cellspacing="0">\
                 <tr>\
-                    <td class="smile"><div class="inner icon-smile"></div></td>\
+                    <td class="smile">\
+                        <div class="inner icon-smile"></div>\
+                    </td>\
                     <td class="message">\
                         <input placeholder="Сообщение" />\
                         <div class="user"></div>\
@@ -23,10 +25,28 @@ function newSendPanel(id) {
                     </td>\
                     <td class="send"><div class="mbutton">Отпр.</div></td>\
                 </tr>\
-            </table>');
+            </table>\
+            <div class="smile-popup">\
+            </div>\
+            '.format(global.chatRoot));
+
+            //<img src="{0}/chat/i/1.gif" data-code="1" />\
 
     self.message = self.workplace.find('.message input');
     self.button = self.workplace.find('.send .mbutton');
+    self.smileButton = self.workplace.find('.smile');
+    self.smilePopup = self.workplace.find('.smile-popup');
+    
+    for (var i = 1; i < 91; i++) {
+        if ((i === 89) || (i === 98) || (i === 99)) continue;
+        var img = $('<img src="{0}/chat/i/{1}.gif" data-code="{1}" />'.format(global.chatRoot, i));
+        binder.tap(img, function() {
+            //self.smilePopup.hide();            
+            self.message.val('{0}(({1}))'.format(self.message.val(),$(this).attr('data-code')));
+        });
+        self.smilePopup.append(img);
+    }
+    
     binder.tap(self.button, function () {
         if (self.onSend) {
             self.send();
@@ -41,7 +61,14 @@ function newSendPanel(id) {
         if (e.keyCode === binder.ENTER_KEY) {
             self.send();
         }
-    });  
+    });
+    
+    binder.tap(self.smileButton, function() {
+        // Немного магии. Площадь занимаемая смайликами делется на ширину экрана и добавляется отступ
+        self.smilePopup.css('height', (128000 / $(window).width()) + 16);
+        self.smilePopup.toggle("fast");
+        self.smileButton.toggleClass("active");        
+    });
     
     self.send = function() {
         var val = self.message.val();
@@ -59,10 +86,6 @@ function newSendPanel(id) {
         inp.css('text-indent', user.outerWidth() + 8);
     };
     
-    self.setTime = function(time) {
-        
-    };
-    
     self.clearUser = function() {
         self.workplace.find('.message .user').hide();
         var inp = self.workplace.find('.message input');
@@ -70,8 +93,12 @@ function newSendPanel(id) {
         if (self.onClearUser !== undefined) self.onClearUser();
     };
     
+    self.setTime = function(time) {
+        // TODO
+    };       
+    
     self.clearTime = function() {
-        
+        // TODO
     };
         
     return self;
