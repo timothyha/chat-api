32db6915e341c423f59f2d03159d950363f0a3fc
Grabli66 <grabli66@gmail.com>
2014-12-30 20:26:32 +0300

    Dialog, refactoring

---
+++ client/www/js/mdialog.js
@@ -0,0 +1,56 @@
+function newDialog() {
+    var self = {};
+    self.onOk = undefined;
+    self.onCancel = undefined;
+    
+    self.workplace = $('<div class="mdialog">\
+                            <div class="inner">\
+                                <div class="title">Ошибка</div>\
+                                <div class="text">Сервер не отвечает</div>\
+                                <table cellpadding="0" cellspacing="0" class="buttons">\
+                                    <tr>\
+                                        <td>\
+                                            <div class="cancel">ВЫЙТИ</div>\
+                                        </td>\
+                                        <td>\
+                                            <div class="ok">ПОВТОРИТЬ</div>\
+                                        </td>\
+                                    </tr>\
+                                </table>\
+                            </div>\
+                           <div class="overlay"></div>\
+                        </div>\
+                          ');
+    $(document.body).append(self.workplace);
+    
+    binder.tap(self.workplace.find('.cancel'), function() {
+        if (self.onCancel !== undefined) self.onCancel();
+        self.hide();
+    });
+    
+    binder.tap(self.workplace.find('.ok'), function() {
+        if (self.onOk !== undefined) self.onOk();
+        self.hide();
+    });
+    
+    self.inner = self.workplace.find('.inner');
+    self.overlay = self.workplace.find('.overlay');
+    
+    self.show = function() {
+        self.inner.show('scale');
+        self.overlay.show();
+    };
+    
+    self.hide = function() {
+        self.inner.hide('scale');
+        self.overlay.hide();
+    };
+    
+    self.resize = function() {
+        self.inner.css('top', ($(window).height() / 2) - self.inner.height() /2);
+        self.inner.css('left', ($(window).width() / 2) - self.inner.width() /2);
+    };
+    
+    self.resize();
+    return self;
+}
